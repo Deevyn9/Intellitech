2fbad8e8714b7e20df871c1e7fe6ed3e403f2662
Divine Obeten <divineobeten9@gmail.com>
2022-11-25 08:04:06 +0100

    making some final touches

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 const navContainer = document.querySelector(".burger-container");
 const linksContainer = document.querySelector(".links-container");
 const navLinks = document.querySelector(".nav-links");
+const activePage = window.location.pathname;
+const navBar = document.querySelector('.main-header')
+const quoteDiv = document.querySelector('.quote__div')
+
+const clickedLinks = document.querySelectorAll('nav a').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active')
+  }
+})
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 80) {
+    navBar.style.position = "fixed";
+    navBar.style.borderBottom = "5px solid #0077B5";
+  } else {
+    navBar.style.position = "relative";
+    navBar.style.borderBottom = "none";
+  }
+});
+
+function ouiMonsieur() {
+  quoteDiv.classList.toggle("appear");
+}
+
+function closeQuote() {
+  quoteDiv.classList.toggle("appear");
+}
 
 function openNav() {
   navContainer.classList.toggle("open");
@@ -15,3 +42,4 @@ function unHide() {
   navLinks.classList.toggle("active");
   document.body.classList.toggle("hidden");
 }
+
